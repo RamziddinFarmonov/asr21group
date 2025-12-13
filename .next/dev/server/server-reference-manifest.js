7dfc049b6470450b7eec987e5ece50d43d45416a
RamziddinFarmonov <ramziddinfarmonov6@gmail.com>
2025-12-13 18:20:16 +0500

    created

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6sAIKNfJzP8DmKs3CsjH2V2AtcxQJRkmvvv/nzMtNJg=\"\n}"
--- .next/dev/static/chunks/app_it-services_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_33bd7e6b._.js",
-  "static/chunks/_7efb2bb7._.js"
-],
-    source: "dynamic"
-});
