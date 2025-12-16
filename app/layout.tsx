40ea4e0730256059afc967c7d4eef5e3b59b221a
RamziddinFarmonov <ramziddinfarmonov6@gmail.com>
2025-12-16 14:10:40 +0500

    changed call number from hujjaruz21 and itServices

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,23 +10,22 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Hujjatuz21 | Professional xizmatlar markazi",
   description: "IT xizmatlari, buxgalteriya va hujjat tayyorlash xizmatlari. Professional jamoa – ishonchli natija.",
-  generator: "v0.app",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/pic/logo.png",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/pic/logo.png",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/pic/logo.png",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "/pic/logo.png",
   },
 }
 
